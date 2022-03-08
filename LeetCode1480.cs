848a044640db296698abfba9c9b5ffa30efcb6b3
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-08 17:35:37 -0500

    Leetcode #1480

---
+++ LeetCode1480.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class LeetCode1480
+    {
+        //LeetCode #1480. Running Sum of 1D Array
+        public int[] RunningSum2(int[] arr)
+        {
+            int[] result = new int[arr.Length];
+            int len = arr.Length;
+
+            result[0] = arr[0];
+            for (int i = 1; i < len; i++) // Begin the loop at position [1]
+            {
+                result[i] = arr[i] + result[i - 1]; // Adding the value at position [i] and the previous value
+            }
+            return result;
+        }
+    }
+}
