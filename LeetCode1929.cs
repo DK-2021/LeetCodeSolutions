ed80cb71d1cd3d23d1a50754e4340c9736d79591
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-08 17:31:13 -0500

    Leetcode #1929

---
+++ LeetCode1929.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class LeetCode1929
+    {
+        // LeetCode #1929. Concatenation of an array.
+        // Duplicating an array and making into in one array.
+        public int[] ConcateArray(int[] inputArray)
+        {
+            int[] result = new int[inputArray.Length * 2];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                result[i] = inputArray[i];
+            }
+            for (int j = 0; j < inputArray.Length; j++)
+            {
+                result[j + inputArray.Length] = inputArray[j]; // Result - at index position (j + arr.length), the remaining value is copied in.
+            }
+            return result;
+        }
+    }
+}
