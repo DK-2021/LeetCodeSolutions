cf23cbb81f2e66b11d5011ec09d05d95bf56c431
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-08 17:15:57 -0500

    LeetCode2160

---
+++ LeetCode2160.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class LeetCode2160
+    {
+        public static int minimumSum(int num)
+        {
+            // LeetCode #2160 (Minimum Sum of Four Digit Number After Splitting Digits)
+            // Taking a givin array and splitting the number, add the digits together to get the lowest sum.
+            
+            int[] arr = new int[4]; // An array for the digits
+            int newNum = 0;
+
+            while (num > 0)
+            {
+                arr[newNum++] = num % 10; 
+                num /= 10; 
+            }
+            Array.Sort(arr);
+            int num1 = arr[0] * 10 + arr[2];
+            int num2 = arr[1] * 10 + arr[3];
+
+            return num1 + num2;
+        }
+    }
+}
