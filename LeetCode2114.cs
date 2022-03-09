bf715747f21404a06b468a464df182b170be7c4f
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-09 08:18:44 -0500

    Leetcode #2114

---
+++ LeetCode2114.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class LeetCode2114
+    {
+        //Leetcode #2114. Maximum Number of Words Found in Sentences.
+        //Split the indexes on the spaces and count the spaces - compare.
+        public int MostWordsFound(string[] sentences)
+        {
+            int result = 0;
+
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                if (sentences[i].Split(" ").Length >= result)
+                {
+                    result = sentences[i].Split(" ").Length;
+                }
+            }
+            return result;
+        }
+    }
+}
