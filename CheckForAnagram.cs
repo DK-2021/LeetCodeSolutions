770801500699180b7545b401941863effd931835
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-09 08:20:51 -0500

    Checking for anagram

---
+++ CheckForAnagram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class CheckForAnagram
+    {
+        // Check if two strings are anagram
+        public static bool CheckAnagram(string x, string y)
+        {
+            char[] ch1 = x.ToLower().ToCharArray();
+            char[] ch2 = y.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string newStr1 = new string(ch1);
+            string newStr2 = new string(ch2);
+            return newStr1.Equals(newStr2, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
