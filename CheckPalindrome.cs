9e8b9dbedac74f8b9ce6136fb83d33b6e34ae08e
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-09 16:20:16 -0500

    Checking if a word is a palindrome

---
+++ CheckPalindrome.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class CheckPalindrome
+    {
+        //Checking to see if a word is a Palindrome
+        public static bool CheckForPalindrome(string userInput)
+        {
+            int badInput; // Check for bad input
+            userInput = userInput.Trim();
+            if (String.IsNullOrWhiteSpace(userInput) || int.TryParse(userInput, out badInput) == true)
+            {
+                return false;
+            }
+            string strOne = "";
+            StringBuilder createdString = new StringBuilder();
+
+            foreach (var i in userInput) // To account for spaces in a word
+            {
+                if (i == ' ')
+                {
+                    continue;
+                };
+                createdString.Append(i);
+            }
+
+            strOne = createdString.ToString();
+
+            int j = strOne.Length - 1;
+            for (int i = 0; i < strOne.Length; i++)
+            {
+                if (strOne[i] != strOne[j])
+                {
+                    return false;
+                }
+                j--;
+            }
+            return true;
+        }
+    }
+}
