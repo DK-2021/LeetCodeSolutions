173379550790fe0351cae49e411bef34a80695da
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-09 16:09:01 -0500

    Is the array distinct

---
+++ IsArrayDistinct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class IsArrayDistinct
+    {
+        // Are there any duplicate numbers in a given array
+        public static bool isDistinctArray(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[j] == input[i])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
