274d855d605dcf2eaae0e005b435e1c49af8b6e2
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-09 16:02:32 -0500

    Leetcode #58 Length of last word

---
+++ LeetCode58.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class LeetCode58
+    {
+        //58 Length of Last Word 
+        public int LengthOfLastWord(string s)
+        {
+            int count = 0, badInput = 0;
+
+            // Check for bad input
+            s = s.Trim();
+            if (String.IsNullOrWhiteSpace(s) || int.TryParse(s, out badInput) == true)
+            {
+                return 0;
+            }
+
+            // Make array and store string
+            string[] userArray = (s.Split(" "));
+
+            // Find length of last index
+            foreach (var i in userArray[userArray.Length - 1])
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
