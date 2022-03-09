4a3770ef24516dc8b005f2adb3da3119297d3827
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-09 16:12:46 -0500

    Adding the digits in a given integer

---
+++ AddingTwoDigits.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class AddingTwoDigits
+    {
+        //Adding two digits of a given integer together 
+        public static int AddTwoDigits(int num)
+        {
+            int totalSum = 0;
+
+            while (num != 0)
+            {
+                totalSum = totalSum + num % 10;
+                num = num / 10;
+            }
+            return totalSum;
+        }
+    }
+}
