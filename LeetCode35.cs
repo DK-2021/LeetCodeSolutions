9eaa0450a047fc1d4749c5f6942a083fdf0c44cd
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-10 07:49:50 -0500

    Leetcode #35 Search the array insert position to match the target

---
+++ LeetCode35.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class LeetCode35
+    {
+        public static int SearchInsert(int[] nums, int target)
+        {
+            //int for while loop
+            int indexResult = 0;
+            //while loop
+            while (indexResult < nums.Length)
+            {
+                //check if target is equal to current index
+                if (target == nums[indexResult])
+                {
+                    return indexResult;
+                }
+                else if (target < nums[indexResult]) //check to see if target is less than current index
+                {
+                    return indexResult;
+                }
+                indexResult++;
+            }
+            //return index if target is found, if not found, return index it would be in
+            return indexResult;
+        }
+    }
+}
