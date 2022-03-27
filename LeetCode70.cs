82f6586abe729d50652af8ac448613ba78fd90f3
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-27 14:21:32 -0400

    Added Solution

---
+++ LeetCode70.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class LeetCode70
+    {
+        //Leetcode #70 Climbing Stairs.
+        //Using Fibonacci (practice again)
+        public int ClimbStairs(int n)
+        {
+            if (n <= 2)
+            {
+                return n;
+            }
+            int[] res = new int[n + 1];
+            res[1] = 1;
+            res[2] = 2;
+
+            for (int i = 3; i <= n; i++)
+            {
+                res[i] = res[i - 1] + res[i - 2];
+            }
+            return res[n];
+        }
+    }
+}
